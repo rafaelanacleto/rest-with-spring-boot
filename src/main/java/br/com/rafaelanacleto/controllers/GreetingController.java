4fbd10024a33ec870c205fe8785d9d999a312ad8
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-05-15 18:38:40 -0300

    Adcionado gitignore

--- src/main/java/br/com/rafaelanacleto/controllers/GreetingController.java
+++ src/main/java/br/com/rafaelanacleto/controllers/GreetingController.java
@@ -2,6 +2,7 @@ package br.com.rafaelanacleto.controllers;
 
 import br.com.rafaelanacleto.model.Greeting;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -13,7 +14,9 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(String name) {
+    public Greeting greeting(
+            @RequestParam(value = "name", defaultValue = "World")
+            String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
